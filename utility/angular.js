94aac3c4e47398d975254b47674da4c429dc8708
Dolan Miu <dolan_miu@hotmail.com>
2016-02-11 16:55:36 +0000

    added utility functions for the task

---
+++ utility/angular.js
@@ -0,0 +1,61 @@
+/*jslint node: true, nomen: true, regexp: true*/
+'use strict';
+
+var _ = require('lodash');
+
+var utility = require('./');
+
+var variableDictionary = {};
+var variableRegex = /(\{\{ ?\w+ ?\}\})/ig;
+var replacedRegex = /(GOOGLE_GRUNT_TRANSLATE_VAR_\d+)/g;
+
+function findVariables(str, regex) {
+    var match = str.match(regex);
+
+    if (match) {
+        return match;
+    } else {
+        return [];
+    }
+}
+
+function addToVariableDictionary(originalString) {
+    var variables = findVariables(originalString, variableRegex),
+        currentString = originalString;
+
+    variables.forEach(function (variable) {
+        var destinationVar = "GRUNT_GOOGLE_TRANSLATE_VAR_" + Object.keys(variableDictionary).length;
+
+        variableDictionary[destinationVar] = variable;
+        currentString = currentString.replace(variable, destinationVar);
+    });
+
+    return currentString;
+}
+
+exports.createVariableSafeJson = function (origJson) {
+    var jsonReferenceArray = [],
+        sourceJson = _.cloneDeep(origJson);
+
+    utility.deepTraverseJson(sourceJson, function (parent, value, key) {
+        var varSafeString = addToVariableDictionary(value);
+        parent[key] = varSafeString;
+    });
+
+    return sourceJson;
+};
+
+exports.revertVariablesInJson = function (translatedJson) {
+    var jsonReferenceArray = [],
+        sourceJson = _.cloneDeep(translatedJson);
+
+    utility.deepTraverseJson(sourceJson, function (parent, value, key) {
+        var variables = findVariables(value, replacedRegex);
+        console.log(variables);
+        //parent[key] = variableDictionary[];
+    });
+
+    return sourceJson;
+};
+
+exports.variableDictionary = variableDictionary;
